c1eb8c6f4d5d9a8cf118e5f20f71a1a7857dbc38
Moh. Absar Rahman <absar.rahman@bjitacademy.com>
2023-02-14 16:47:35 +0600

    Animate homeview while scrolling
    Signed-off-by: Moh. Absar Rahman <absar.rahman@bjitacademy.com>

--- Cricketly/ViewModels/HomeViewController.swift
+++ Cricketly/ViewModels/HomeViewController.swift
@@ -11,7 +11,21 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     @IBOutlet weak var bannerView: UIView!
+    
+    
+    @IBOutlet weak var bannerTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var matchesLabelTopConstraint: NSLayoutConstraint!
+    
+    var lastContentOffset: CGFloat = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +35,16 @@ class HomeViewController: UIViewController {
         UIFunctions.setViewCornerRadius(view: bannerView, cornerRadius: 20, edgeType: [.layerMaxXMinYCorner,.layerMaxXMaxYCorner])
         
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: collectionView.bounds.width * 0.8, height: 210)
+        layout.itemSize = CGSize(width: collectionView.bounds.width * 0.8, height: collectionView.bounds.height * 0.9)
         layout.scrollDirection = .horizontal
         layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
         collectionView.collectionViewLayout = layout
         collectionView.dataSource = self
         
         collectionView.register(UINib(nibName: FixturesCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: FixturesCollectionViewCell.identifier)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 }
@@ -42,6 +59,51 @@ extension HomeViewController : UICollectionViewDataSource {
         
         return cell
     }
+}
+
+
+extension HomeViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Yeet"
+        return cell
+    }
     
+}
+extension HomeViewController: UITableViewDelegate, UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            UIView.animate(withDuration: 0.1, delay: 0) {[weak self] in
+                guard let self = self else {
+                    return
+                }
+                if (100 > scrollView.contentOffset.y) {
+                    print("Scrolled up")
+                    
+                    if (self.matchesLabelTopConstraint.constant < 180) {
+                        self.bannerTopConstraint.constant += 15
+                        self.matchesLabelTopConstraint.constant += 10
+                    } else {
+                        self.matchesLabelTopConstraint.constant = 180
+                        self.bannerTopConstraint.constant = 80
+                    }
+                    
+                } else if (100 < scrollView.contentOffset.y) {
+                    print("Scrolled down")
+                    if (self.matchesLabelTopConstraint.constant > 0) {
+                        self.bannerTopConstraint.constant -= 15
+                        self.matchesLabelTopConstraint.constant -= 10
+                    } else {
+                        self.matchesLabelTopConstraint.constant = 0
+                        self.bannerTopConstraint.constant = -500
+                    }
+                    
+                }
+            }
+            
+            //self.lastContentOffset = scrollView.contentOffset.y
+        }
 }
