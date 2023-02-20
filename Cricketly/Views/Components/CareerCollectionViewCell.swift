a06ab0e6961692ba983776c5545dc318946dc21a
Moh. Absar Rahman <absar.rahman@bjitacademy.com>
2023-02-20 20:52:09 +0600

    Initial design of career collection cell
    Signed-off-by: Moh. Absar Rahman <absar.rahman@bjitacademy.com>

---
+++ Cricketly/Views/Components/CareerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CareerCollectionViewCell.swift
+//  Cricketly
+//
+//  Created by BJIT  on 20/2/23.
+//
+
+import UIKit
+
+class CareerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
